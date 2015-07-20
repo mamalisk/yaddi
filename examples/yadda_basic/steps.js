063fe74a5978db818baf2a0f6f1edbefc7e7d7ef
Kostas Mamalis <kostas@masterthought.net>
2015-07-20 17:04:19 +0100

    example of several step definition classes

--- examples/yadda_basic/steps.js
+++ examples/yadda_basic/steps.js
@@ -2,27 +2,15 @@
 
 var yadda = require('yadda').localisation.English.library();
 
+var Yadda1 = require('./yadda1');
+var Yadda2 = require('./yadda2');
+var all = new Yadda2(new Yadda1({
+    yadda : yadda
+}));
+
+
 module.exports = (function(){
 
-    return yadda 
-
-        .given("I am rich", function (done){
-        	console.log("i am rich");
-        	done();
-        })
-
-        .when("I buy a Porsche", function(done){
-        	console.log('i buy a porsche');
-        	done();
-        })
-
-        .when('I cause a failure', function(done){
-        	throw new Error('failure!!!');
-        })
-
-        .then("I am happier", function(done){
-            console.log('i am happy');
-            done();
-        });
+    return all.yadda;
    
 })();
---
+++ examples/yadda_basic/yadda1.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var secondSteps = function(yadda){
+
+    this.yadda = yadda.yadda;
+
+    this.yadda
+
+        .given("I am too rich", function (done){
+            console.log("i am too rich");
+            done();
+        });
+};
+
+
+module.exports = secondSteps;
